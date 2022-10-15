28e5db3c74dcaaa256974c0e83b5bb13f509efc9
ajeskepuh <aziznasrul85@gmail.com>
2022-10-15 19:05:48 +0700

    v0.02

--- file.js
+++ file.js
@@ -1,9 +1,19 @@
 function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("geser").style.marginLeft = "250px";
+    document.getElementById("mySidebar").style.width = "160px";
+    document.getElementById("geser").style.marginLeft = "0px";
   }
   
   function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("geser").style.marginLeft= "0";
   }
+
+  const checkbox = document.getElementById('checkbox');
+
+checkbox.addEventListener('change', ()=>{
+  document.body.classList.toggle('dark');
+})
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark");
+}
