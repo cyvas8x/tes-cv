611da5e56045c09aa3014df3b85207995121e61a
ajeskepuh <aziznasrul85@gmail.com>
2022-10-19 03:48:47 +0700

    fix

--- file.js
+++ file.js
@@ -1,11 +1,13 @@
 /* SIDEBAR */
 function openNav() {
   document.getElementById("mySidebar").style.width = "210px";
+  document.getElementById("mySidebar").style.borderRight = "2px solid yellow"
   document.getElementById("geser").style.marginLeft = "0px";
 }
 
 function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("mySidebar").style.borderRight = "0"
   document.getElementById("geser").style.marginLeft= "0";
 }
 
