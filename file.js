c0134c7158df6d0753eb7e41b2145900c1c527b8
ajeskepuh <aziznasrul85@gmail.com>
2022-10-19 03:06:26 +0700

    tambah dark mode

--- file.js
+++ file.js
@@ -1,19 +1,11 @@
+/* SIDEBAR */
 function openNav() {
-    document.getElementById("mySidebar").style.width = "160px";
-    document.getElementById("geser").style.marginLeft = "0px";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("geser").style.marginLeft= "0";
-  }
-
-  function darkmode() {
-    if (document.getElementById('darkmode-toggle').checked) {
-      var element = document.body;
-      element.classList.toggle("dark");   
-    } else {
-        var element = document.body;
-        element.classList.toggle("normal")
-    }
+  document.getElementById("mySidebar").style.width = "160px";
+  document.getElementById("geser").style.marginLeft = "0px";
 }
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("geser").style.marginLeft= "0";
+}
+
